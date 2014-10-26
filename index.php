26f23772bc5a7e349c23866dddcb3174c3ab6050
Alberto Berroteran <aberroteran@live.com>
2014-10-26 17:50:38 -0430

    prism

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <head>
     <link rel="stylesheet" type="text/css" href="/doc/style.css" />
+    <link href="/doc/prism.css" rel="stylesheet" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
 </head>
 <body>
 <?php
@@ -12,10 +12,13 @@ $instance = new Markdown();
 
 $text = file_get_contents($file);
 ?>
+<?php if (!preg_match('/index.md$/', $file)):?>
 <div class="toc">
 <?php echo $instance->text(toc_file($file, FALSE));?>
 </div>
+<?php endif;?>
 
 <?php echo $instance->text($text);?>
+<script src="/doc/prism.js"></script>
 </body>
 </html>
