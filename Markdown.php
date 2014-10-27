418e3c42a0551c407599220698a956b5f8d11667
Alberto Berroteran <aberroteran@live.com>
2014-10-26 19:55:27 -0430

    Now images is available

--- Markdown.php
+++ Markdown.php
@@ -8,10 +8,20 @@ class Markdown extends ParsedownExtra{
         return $array;
     }
 
+
+
     static public function generateId($title){
         return  str_replace(' ',  '-', 
             str_replace(array('(', ')', '?', '¿'), '', strtolower($title))
         );        
+    }
+
+    protected function identifyLink($Excerpt){
+        $r = parent::identifyLink($Excerpt);
+        if(isset($r['element']['name']) && $r['element']['name'] == 'img'){
+            $r['element']['attributes']['src'] =  '/doc/'.$r['element']['attributes']['src'];
+        }
+        return $r;
     }
 
 }
