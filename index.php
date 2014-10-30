3b5935e09123e874c3240e67ba126493af5287d1
Alberto Berroteran <aberroteran@live.com>
2014-10-30 10:15:54 -0430

    utf8 and flexible url

--- index.php
+++ index.php
@@ -3,12 +3,14 @@ include 'toc.php';
 $file = empty($_GET['file']) ? 'readme.md':$_GET['file'];
 $instance = new Markdown();
 $text = file_get_contents($file);
+$url = substr($_SERVER['PHP_SELF'],0,-9);
 ?>
 <!DOCTYPE html>
 <head>
-    <link rel="stylesheet" type="text/css" href="/doc/style.css" />
-    <link href="/doc/prism.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="<?php echo $url;?>style.css" />
+    <link href="<?php echo $url;?>prism.css" rel="stylesheet" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />.
     <?php preg_match('/^\s*(#{1,})(.*)/m', $text, $title);?>
     <title><?php echo $title[2];?></title>
 </head>
@@ -21,6 +23,6 @@ $text = file_get_contents($file);
 <?php endif;?>
 
 <?php echo $instance->text($text);?>
-<script src="/doc/prism.js"></script>
+<script src="<?php echo $url;?>prism.js"></script>
 </body>
 </html>
